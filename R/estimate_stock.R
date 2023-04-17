78bf07d2b2dc0770213334ada1a51cbe11b55ab5
NPJuncal <npjuncal@gmail.com>
2023-04-17 14:34:45 +0100

    Allow to especify the name of the column with the data and, instead of estimaten the thikness of the sample used function estimate_h

--- R/estimate_stock.R
+++ R/estimate_stock.R
@@ -5,39 +5,35 @@
 #'
 #' @param df A [data.frame] with, at least, columns: Core.ID, Min.D (minimum depth of the sample), Max.D (maximum depth of the sample), DBD (dry bulk density), POC (organic carbon %)
 #' @param Depth standardization soil depth, by default 100 cm.
+#' @param Core.ID the name of the column (between "") from the df with the Core identification for each sample
+#' @param DMin the name of the column (between "") from the df with the minimum depth of that sample (already corrected if needed)
+#' @param DMax the name of the column (between "") from the df with the maximum depth of that sample (already corrected if needed)
+#' @param DBD the name of the column (between "") from the df with the dry bulk density of that sample (already corrected if needed)
+#' @param POC the name of the column (between "") from the df with the percentage of organic carbon in the sample
 #'
 #' @return [data.frame] with columns Core.id, S.WC (organic carbon stock at the whole core), D.Max (maximum depth of the core), and Stock (organic carbon stock at the standardized depth)
 #' @export
 #'
 #' @examples
 
-estimate_stock <- function(df = NULL, Depth = 100) {
+estimate_stock <- function(df = NULL, Depth = 100,  Core.ID="Core.ID", DMin= "DMin", DMax="DMax", DBD= "DBD", POC="POC") {
 
   # class of the dataframe
   if (is.data.frame(df)==FALSE) {stop("The data provided is not class data.frame, please chaeck data and transforme")}
   if (is.numeric(Depth)==FALSE) {stop("The Depth provided is not class numeric, please chaeck data and transforme")}
 
-  # name of the columns
-  if ("CoreID" %in% colnames(df)==FALSE) {stop("There is not column named CoreID. Please, check necessary columns in functions documentation")}
-  if ("Min.D" %in% colnames(df)==FALSE) {stop("There is not column named Min.D. Please, check necessary columns in functions documentation")}
-  if ("Max.D" %in% colnames(df)==FALSE) {stop("There is not column named Max.D. Please, check necessary columns in functions documentation")}
-  if ("DBD" %in% colnames(df)==FALSE) {stop("There is not column named DBD. Please, check necessary columns in functions documentation")}
-  if ("POC" %in% colnames(df)==FALSE) {stop("There is not column named POC. Please, check necessary columns in functions documentation")}
-
-  # class of the columns
-  if (is.numeric(df$Min.D)==FALSE) {stop("Minimum depth data is not class numeric, please chaeck")}
-  if (is.numeric(df$Max.D)==FALSE) {stop("Maximum depth data is not class numeric, please chaeck")}
-  if (is.numeric(df$DBD)==FALSE) {stop("Dry Bulk Density data is not class numeric, please chaeck")}
-  if (is.numeric(df$POC)==FALSE) {stop("Organic carbon data is not class numeric, please chaeck")}
+  df2<-as.data.frame(cbind(df[[Core.ID]], df[[DMin]],df[[DMax]],df[[DBD]], df[[POC]]))
+  colnames(df2)<-c("Core.ID","DMin","DMax","DBD","POC")
+  df2[, 2:5] <- sapply(df2[, 2:5], as.numeric)
 
 
-  df <-df |> mutate (Center = Min.D+((Max.D-Min.D)/2))
+  df3<-estimate_h (df2, Core.ID=Core.ID, DMin= "DMin", DMax="DMax")
 
-  df = filter(df, !is.na(POC))
+  df3 = filter(df3, !is.na(POC))
 
-  X<-split(df, df$CoreID)
+  X<-split(df3, df3$Core.ID)
 
-  BCS <- data.frame(CoreID=character(),
+  BCS <- data.frame(Core.ID=character(),
                     S.WC=numeric(),
                     D.Max=numeric(),
                     Stock=numeric())
@@ -45,57 +41,40 @@ estimate_stock <- function(df = NULL, Depth = 100) {
   for(i in 1:length(X)) {
     BCS[i,1]<-names(X[i])
     Data<-as.data.frame(X[i])
-    colnames(Data)<-colnames(df)
-
-
+    colnames(Data)<-colnames(df3)
 
     if(nrow(Data)<3) next
 
     else{
 
-      Data$h<-NA
-
-      #estimation of the thickness of the sample (h)
-      for (j in 2:(nrow(Data)-1)) {
-
-        Data[j,which(colnames(Data)=="h")]<-
-          ((Data[j,which(colnames(Data)=="Center")]-Data[(j-1),which(colnames(Data)=="Center")])/2)+
-          ((Data[(j+1),which(colnames(Data)=="Center")]-Data[j,which(colnames(Data)=="Center")])/2)
-
-      }
-
-      Data[1,which(colnames(Data)=="h")]<-Data[1,which(colnames(Data)=="Center")]
-      Data[nrow(Data),which(colnames(Data)=="h")]<-
-        Data[nrow(Data),which(colnames(Data)=="Max.D")]-Data[nrow(Data),which(colnames(Data)=="Center")]
-
       #estimation of carbon g cm2 per sample, OCgcm2= carbon density (g cm3) by thickness (h)
-      Data <-Data |> mutate (OCgcm2 = DBD*(POC/100)*h)
+      Data <-Data |> dplyr::mutate (OCgcm2 = DBD*(POC/100)*h)
 
       #estimation of the OC stoack in the whole core
       BCS[i,2]<-sum(Data[,which(colnames(Data)=="OCgcm2")])
-      BCS[i,3]<-max(Data[,which(colnames(Data)=="Max.D")])
+      BCS[i,3]<-max(Data[,which(colnames(Data)=="EMax")])
 
       #if core exactly the standarization depth, we keep the stock of the whole core
-      if(max(Data$Max.D)==Depth) {BCS[i,4]<-sum(Data[,which(colnames(Data)=="OCgcm2")])}
+      if(max(Data$EMax)==Depth) {BCS[i,4]<-sum(Data[,which(colnames(Data)=="OCgcm2")])}
 
 
       else{
 
         # if the core longer than the standarization depth we estimate the stock until 1m depth
-        if (max(Data$Max.D)>=Depth)
+        if (max(Data$EMax)>=Depth)
 
         {
-          Data<-Data[c(1:(length(which(Data$Max.D <=Depth))+1)),]
+          Data<-Data[c(1:(length(which(Data$EMax <=Depth))+1)),]
 
           BCS[i,4]<-(((sum(Data[c(1:(nrow(Data)-1)),which(colnames(Data)=="OCgcm2")]))+
-                        (Data[nrow(Data),which(colnames(Data)=="OCgcm2")]/((max(Data$Max.D)-Data[(nrow(Data)-1),which(colnames(Data)=="Max.D")]))
-                         *(Depth-Data[(nrow(Data)-1),which(colnames(Data)=="Max.D")]))))}
+                        (Data[nrow(Data),which(colnames(Data)=="OCgcm2")]/((max(Data$EMax)-Data[(nrow(Data)-1),which(colnames(Data)=="EMax")]))
+                         *(Depth-Data[(nrow(Data)-1),which(colnames(Data)=="EMax")]))))}
 
         #if core shorter than than the standarization depth we model the OC acumulated mass with depth and predict the stock at 1m
         else {
 
-          Data <-Data |> mutate (OCM = cumsum(OCgcm2))
-          model<-lm(OCM ~ Max.D, data=Data)
+          Data <-Data |> dplyr::mutate (OCM = cumsum(OCgcm2))
+          model<-lm(OCM ~ EMax, data=Data)
           BCS[i,4]<-coef(model)[1] + Depth*coef(model)[2]}}
     }}
   return(BCS)
