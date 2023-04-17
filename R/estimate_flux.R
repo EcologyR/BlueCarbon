7b93fddbc7290f631ea35d9e767e092a95b11701
NPJuncal <npjuncal@gmail.com>
2023-04-17 14:54:51 +0100

    Allows to chose the name of the columns. Now it uses the function estimate_h the estimate sample thickness

--- R/estimate_flux.R
+++ R/estimate_flux.R
@@ -4,6 +4,12 @@
 #'
 #' @param df A [data.frame] with, at least, columns: Core.ID, Min.D (minimum depth of the sample), Max.D (maximum depth of the sample), DBD (dry bulk density), POC (organic carbon %), Age (age of the sample obtained from a age-depth or age-accumulated mass model)
 #' @param TimeFrame standardization time frame, by default 100 years
+#' @param Core.ID the name of the column (between "") from the df with the Core identification for each sample
+#' @param DMin the name of the column (between "") from the df with the minimum depth of that sample (already corrected if needed)
+#' @param DMax the name of the column (between "") from the df with the maximum depth of that sample (already corrected if needed)
+#' @param DBD the name of the column (between "") from the df with the dry bulk density of that sample (already corrected if needed)
+#' @param POC the name of the column (between "") from the df with the percentage of organic carbon in the sample
+#' @param Age the name of the column (between "") from the df with the percentage of organic carbon in the sample
 #'
 #' @return [data.frame] with columns Core.id, F.WC (organic carbon fluxes at the whole core), A.Max (maximum age of the core), and Flux (average organic carbon flux at the indicated time frame)
 #' @export
@@ -11,38 +17,26 @@
 #' @examples
 
 
-estimate_flux<- function(df=NULL,TimeFrame=100) {
+estimate_flux<- function(df=NULL,TimeFrame=100, Core.ID="Core.ID", DMin= "DMin", DMax="DMax", DBD= "DBD", POC="POC", Age="Age") {
 
   # class of the dataframe
   if (is.data.frame(df)==FALSE) {stop("The data provided is not class data.frame, please chaeck data and transforme")}
   if (is.numeric(TimeFrame)==FALSE) {stop("The TimeFrame provided is not class numeric, please chaeck data and transforme")}
 
-  # name of the columns
-  if ("CoreID" %in% colnames(df)==FALSE) {stop("There is not column named CoreID. Please, check necesary columns in functions documentation")}
-  if ("Min.D" %in% colnames(df)==FALSE) {stop("There is not column named Min.D. Please, check necesary columns in functions documentation")}
-  if ("Max.D" %in% colnames(df)==FALSE) {stop("There is not column named Max.D. Please, check necesary columns in functions documentation")}
-  if ("DBD" %in% colnames(df)==FALSE) {stop("There is not column named DBD. Please, check necesary columns in functions documentation")}
-  if ("POC" %in% colnames(df)==FALSE) {stop("There is not column named POC. Please, check necesary columns in functions documentation")}
-  if ("Age" %in% colnames(df)==FALSE) {stop("There is not column named Age. Please, check necesary columns in functions documentation")}
 
-  # class of the columns
-  if (is.numeric(df$Min.D)==FALSE) {stop("Minimum depth data is not class numeric, please chaeck")}
-  if (is.numeric(df$Max.D)==FALSE) {stop("Maximum depth data is not class numeric, please chaeck")}
-  if (is.numeric(df$DBD)==FALSE) {stop("Dry Bulk Density data is not class numeric, please chaeck")}
-  if (is.numeric(df$POC)==FALSE) {stop("Organic carbon data is not class numeric, please chaeck")}
-  if (is.numeric(df$Age)==FALSE) {stop("Age data is not class numeric, please chaeck")}
-
-
-  df <-df |> mutate (Center = Min.D+((Max.D-Min.D)/2))
+  df2<-as.data.frame(cbind(df[[Core.ID]], df[[DMin]],df[[DMax]],df[[DBD]], df[[POC]], df[[Age]]))
+  colnames(df2)<-c("Core.ID","DMin","DMax","DBD","POC", "Age")
+  df2[, 2:6] <- sapply(df2[, 2:6], as.numeric)
 
   #select those cores with chronological models
-  AD = filter(df, !is.na(Age))
+  df3 = filter(df2, !is.na(Age))
+  df4 = filter(df3, !is.na(POC))
 
-  length(unique(AD$CoreID))# number of cores with age estimations
+  df4<-estimate_h (df4, Core.ID=Core.ID, DMin= "DMin", DMax="DMax")
 
-  X<-split(AD, AD$CoreID)
+  X<-split(df4, df4$Core.ID)
 
-  BCF <- data.frame(CoreID=character(),
+  BCF <- data.frame(Core.ID=character(),
                     F.WC=numeric(),
                     A.Max=numeric(),
                     Flux=numeric())
@@ -51,31 +45,13 @@ estimate_flux<- function(df=NULL,TimeFrame=100) {
   for(i in 1:length(X)) {
     BCF[i,1]<-names(X[i])
     Data<-as.data.frame(X[i])
-    colnames(Data)<-colnames(AD)
-
-
-    Data = filter(Data, !is.na(POC))
+    colnames(Data)<-colnames(df4)
 
     if(nrow(Data)<3) next
 
     else{
 
-      Data$h<-NA
-
-      #estimation of the thickness of the sample (h)
-      for (j in 2:(nrow(Data)-1)) {
-
-        Data[j,which(colnames(Data)=="h")]<-
-          ((Data[j,which(colnames(Data)=="Center")]-Data[(j-1),which(colnames(Data)=="Center")])/2)+
-          ((Data[(j+1),which(colnames(Data)=="Center")]-Data[j,which(colnames(Data)=="Center")])/2)
-      }
-
-      #estimation of carbon g cm2 per sample, OCgcm2= carbon density (g cm3) by thickness (h)
-      Data[1,which(colnames(Data)=="h")]<-Data[1,which(colnames(Data)=="Center")]
-      Data[nrow(Data),which(colnames(Data)=="h")]<-
-        Data[nrow(Data),which(colnames(Data)=="Max.D")]-Data[nrow(Data),which(colnames(Data)=="Center")]
-
-      Data <-Data |> mutate (OCgcm2 = DBD*(POC/100)*h)
+      Data <-Data |> dplyr::mutate (OCgcm2 = DBD*(POC/100)*h)
 
       #estimation of the average carbon flux for the whole core (OC stock/Max Age)
       BCF[i,2]<-(sum(Data[,which(colnames(Data)=="OCgcm2")]))/max(Data$Age)
