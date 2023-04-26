6c94a2443c207c8139cb81da9ac74bece7e24eb4
NPJuncal <npjuncal@gmail.com>
2023-04-26 16:37:38 +0100

    Changes in the warnings

--- R/test_extrapolation.R
+++ R/test_extrapolation.R
@@ -18,8 +18,8 @@ test_extrapolation <- function(df = NULL, Depth = 100) {
 
   # name of the columns
   if ("CoreID" %in% colnames(df)==FALSE) {stop("There is not column named CoreID. Please, check necessary columns in functions documentation")}
-  if ("DMin" %in% colnames(df)==FALSE) {stop("There is not column named Min.D. Please, check necessary columns in functions documentation")}
-  if ("DMax" %in% colnames(df)==FALSE) {stop("There is not column named Max.D. Please, check necessary columns in functions documentation")}
+  if ("DMin" %in% colnames(df)==FALSE) {stop("There is not column named DMin. Please, check necessary columns in functions documentation")}
+  if ("DMax" %in% colnames(df)==FALSE) {stop("There is not column named DMax. Please, check necessary columns in functions documentation")}
   if ("DBD" %in% colnames(df)==FALSE) {stop("There is not column named DBD. Please, check necessary columns in functions documentation")}
   if ("fOC" %in% colnames(df)==FALSE) {stop("There is not column named fOC. Please, check necessary columns in functions documentation")}
 
--- tests/testthat/test-estimate_stock.R
+++ tests/testthat/test-estimate_stock.R
@@ -2,7 +2,7 @@
 
 test_that("Carbon stock correctelly estimated", {
 
-  df<-data.frame(CoreID="A", Min.D=c(0,20,40,60,80), Max.D=c(20,40,60,80,100), DBD=1, POC=2)
+  df<-data.frame(CoreID="A", DMin=c(0,20,40,60,80), DMax=c(20,40,60,80,100), DBD=1, fOC=2)
 
 
   expect_equal(estimate_stock(df)[1,2]==1.6, TRUE)
