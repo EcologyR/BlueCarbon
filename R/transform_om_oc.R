7d1f77066ae73ea23643d6c19537ed98c8c0a624
Julen Astigarraga <julenastigarraga@gmail.com>
2023-05-11 12:33:02 +0200

    functions 1 & 2 with apply

--- R/transform_om_oc.R
+++ R/transform_om_oc.R
@@ -3,7 +3,6 @@
 #' @description Model linear relation between organic matter and organic carbon and estimate organic carbon values from organic matter data
 #'
 #' @param df A tibble or data.frame with, at least, columns CoreID, Ecosystem, Species, SiteID, OM, and OC.
-#' #### FRS: An alternative is to let the user specify the column names for each of these things
 #'
 #' @return the initial data.frame + one column with organic carbon values (fOC = final organic carbon)
 #' @export
@@ -12,10 +11,10 @@
 
 load("data/DataInv.rda")
 
+# function 1: split --------------------------------------------------------------
 # create list of dataframes with data from each ecosystem, species, and station (site)
 split_ecosystem <- function(df = NULL,
                             site = NULL,
-                            core = NULL,
                             ecosystem = NULL,
                             species = NULL,
                             om = NULL,
@@ -27,7 +26,6 @@ split_ecosystem <- function(df = NULL,
 
   # names of the columns
   if (!site %in% names(df)) {stop("There must be a column named 'SiteID'")}
-  if (!core %in% names(df)) {stop("There must be a column named 'CoreID'")}
   if (!ecosystem %in% names(df)) {stop("There must be a column named 'Ecosystem'")}
   if (!species %in% names(df)) {stop("There must be a column named 'Species'")}
   if (!om %in% names(df)) {stop("There must be a column named 'OM'")}
@@ -37,102 +35,115 @@ split_ecosystem <- function(df = NULL,
   if (!is.numeric(df[[om]])) {stop("Organic matter data must be class numeric")}
   if (!is.numeric(df[[oc]])) {stop("Organic carbon data must be class numeric")}
 
-
   # check there are no negative values
+  # if (min(df[[om]], na.rm = T) < 0) {stop("There are organic matter negative values")}
+  if (min(df[[oc]], na.rm = T) < 0) {stop("There are organic carbon negative values")}
 
-  if (min(df$om)<0) {stop("There are organic matter negative values")}
-  if (min(df$oc)<0) {stop("There are organic carbon negative values")}
+  return(split(df, df[[ecosystem]]))
 
+}
 
 ecosystem_ls <- split_ecosystem(
   df = DataInv,
   site = "SiteID",
-  core = "CoreID",
   ecosystem = "Ecosystem",
   species = "Specie",
   om = "OM",
   oc = "OC"
 )
 
+# function 2: fit models --------------------------------------------------------------
+# estimate df linear model to predict OC from OM for each ecosystem, species and station
+fit_models <- function(df = NULL,
+                       site = NULL,
+                       species = NULL,
+                       om = NULL,
+                       oc = NULL) {
 
-transform_om_oc <- function(df = NULL,
-                            site = NULL,
-                            core = NULL,
-                            ecosystem = NULL,
-                            species = NULL,
-                            om = NULL,
-                            oc = NULL) {
+  df_r <- df
+  df_r$site_r <- df_r[[site]]
+  df_r$species_r <- df_r[[species]]
+  df_r$core_r <- df_r[[core]]
+  df_r$ecosystem_r <- df_r[[ecosystem]]
+  df_r$om_r <- df_r[[om]]
+  df_r$oc_r <- df_r[[oc]]
 
-  #### Estimate df linear model to predict OC from OM for each ecosystem, species and station ###
+  df_r <- df_r[!is.na(df_r$oc_r), ]
+  df_r <- df_r[!is.na(df_r$om_r), ]
 
-  # check if the class of the parameters objects, the names and class of the columns of df are correct
+  # if there are 0 in the data we add 0.00001 or the logarithm will not work
+  if (0 %in% df_r$om_r) {df_r$om_r <- df_r$om_r + 0.00001}
+  if (0 %in% df_r$oc_r) {df_r$oc_r <- df_r$oc_r + 0.00001}
 
+  if (nrow(df_r) > 10){
 
-  #for each ecosystem
-  df <- ecosystem_ls[["Seagrass"]]
-  View(df)
-
-  fit_ecosystem_models<- function (df) {
-
-    df<-df[!is.na(df[[oc]]),]
-    df<-df[!is.na(df[[om]]),]
-
-
-    # if there are 0 in the data we add 0.00001 or the logaritm will not work
-    if (0 %in% df[[om]]) {df[[om]]<-df[[om]]+0.00001}
-    if (0 %in% df[[oc]]) {df[[oc]]<-df[[oc]]+0.00001}
-
-    if (nrow(df)>10){
-
-
-   # ecosystem model --------------------------------------------------------------
-  fit_full_model <- function(x) { # function to estimate a model with all samples from that ecosystem, the full model
-    lm(log(x[[oc]]) ~ log(x[[om]]))
-  }
-
-
-  ecosystem_model <- fit_full_model (df)
-
-
-  # multispecies model -----------------------------------------------------------
-
-  fit_multispecies_model <- function(x) {# estimates a different model per specie withing the ecosystem
-    if (length(unique(x[[species]])) > 1) {
-      lm(log(x[[oc]]) ~ log(x[[om]]) *
-           x[[species]])
-    }
-  }
-
-  multispecies_model <- fit_multispecies_model(df)
-
-# site model --------------------------------------------------------------
-
-  split_species <- function(df) {# divide the data.frame per specie
-    species_ls <- split(df, df[[species]])
-  }
-  species_ls <- split_species (df)
-
-  fit_site_model <- function(x) {# model with data from each specie dependent on sampling site
-    if (length(unique(x[[site]])) > 1) {
-      lm(log(x[[oc]]) ~ log(x[[om]]) *
-           x[[site]])
-    } else {
-      lm(log(x[[oc]]) ~ log(x[[om]]))
-    }
-  }
-
-  site_models <- lapply(X = species_ls, FUN = fit_site_model)
-
-  output<-list(ecosystem_model, multispecies_model, site_models)
-  names(output)<-c("ecosystem_model", "multispecies_model", "site_models")
-
-   return(output)}
-
+    # ecosystem model --------------------------------------------------------------
+    # function to estimate a model with all samples from_r that ecosystem, the full model
+    fit_ecosystem_models <- function(x) {
+      lm(log(oc_r) ~ log(om_r), data = x)
     }
 
-  all_models<-lapply( X = ecosystem_ls, FUN = fit_ecosystem_models)
+    ecosystem_model <- fit_ecosystem_models(x = df_r)
 
+    # multispecies model -----------------------------------------------------------
+    # estimates a different model per specie withing the ecosystem
+    fit_multispecies_model <- function(x) {
+      if (length(unique(df_r$species_r)) > 1) {
+        lm(log(oc_r) ~ log(om_r) *
+             species_r, data = x)
+      }
+    }
 
+    multispecies_model <- fit_multispecies_model(x = df_r)
 
+    # site model --------------------------------------------------------------
+
+    # divide the data.frame per specie
+    split_species <- function(x) {
+      species_ls <- split(x, x$species_r)
+    }
+
+    species_ls <- split_species(x = df_r)
+
+    # model with data from_r each specie dependent on sampling site
+    fit_site_model <- function(x) {
+      if (length(unique(site_r)) > 1) {
+        lm(log(oc_r) ~ log(om_r) *
+             site_r, data = x)
+      } else {
+        lm(log(oc_r) ~ log(om_r), data = x)
+      }
+    }
+
+    site_models <- lapply(X = species_ls, FUN = fit_site_model)
+
+    output <- list(ecosystem_model, multispecies_model, site_models)
+
+    names(output) <- c("ecosystem_model", "multispecies_model", "site_models")
+
+    return(output)
+
+  }
 }
+
+all_models <- purrr::map(ecosystem_ls, \(x) fit_models(
+  x,
+  site = "SiteID",
+  species = "Specie",
+  om = "OM",
+  oc = "OC"
+))
+
+View(all_models)
+
+all_models <- lapply(
+  X = ecosystem_ls,
+  FUN = fit_models,
+  site = "SiteID",
+  species = "Specie",
+  om = "OM",
+  oc = "OC"
+)
+
+View(all_models)
 
