4a0d0a7123abda9aa1b83244b092a3261442d334
NPJuncal <npjuncal@gmail.com>
2023-05-12 08:47:41 +0100

    check data ejemplo

--- data-raw/exampledata_prep.R
+++ data-raw/exampledata_prep.R
@@ -1,4 +1,4 @@
-DataInv <- read.csv("data-raw/bluecarbon_data.csv", sep= ";")
+bluecarbon_data <- read.csv("data-raw/bluecarbon_data.csv", sep= ";")
 
 
-usethis::use_data(DataInv, overwrite = TRUE)
+usethis::use_data(bluecarbon_data, overwrite = TRUE)
