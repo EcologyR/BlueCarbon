6e510c065e27b1c00b5da73b926d80de7d43e3e9
NPJuncal <npjuncal@gmail.com>
2023-06-10 19:24:18 +0100

    names can be chosen by the user #23

--- R/estimate_flux.R
+++ R/estimate_flux.R
@@ -3,7 +3,7 @@
 #' @description estimate the average organic carbon flux to the soil in a indicated time frame (by default last 100 years) from the organic carbon concentration and ages obtained from a age-depth or age-accumulated mass model
 #'
 #' @param df A [data.frame] with, at least, columns: Core.ID, DMin (minimum depth of the sample), DMax (maximum depth of the sample), DBD (dry bulk density), POC (organic carbon %), Age (age of the sample obtained from a age-depth or age-accumulated mass model)
-#' @param TimeFrame standardization time frame, by default 100 years
+#' @param timeframe standardization time frame, by default 100 years
 #' @param core Character Name of the column reporting core ID.
 #' @param mind Character Name of the column reporting the minimum depth of each sample.
 #' @param maxd Character Name of the column reporting the maximum depth of each sample.
@@ -19,10 +19,10 @@
 
 
 estimate_flux<- function(df=NULL,
-                         TimeFrame=100,
+                         timeframe=100,
                          core = "core",
-                         dmin = "dmin",
-                         dmax = "dmax",
+                         mind = "mind",
+                         maxd = "maxd",
                          dbd = "dbd",
                          oc = "eoc",
                          age = "age") {
@@ -31,19 +31,19 @@ estimate_flux<- function(df=NULL,
   if (!inherits(df, "data.frame")) {
     stop("The data provided must be a tibble or data.frame")
   }
-  if (!is.numeric(Depth)) {stop("The Depth provided is not class numeric, please chaeck data and transforme")}
+  if (!is.numeric(timeframe)) {stop("The time frame must be class numeric")}
 
   # name of the columns
-  if (!core %in% colnames(df)) {stop("There must be a variable with 'core'")}
-  if (!dmin %in% colnames(df)) {stop("There must be a variable with 'dmin'")}
-  if (!dmax %in% colnames(df)) {stop("There must be a variable with 'dmax'")}
-  if (!dbd %in% colnames(df)) {stop("There must be a variable with 'dbd'")}
-  if (!oc %in% colnames(df)) {stop("There must be a variable with 'oc'")}
-  if (!age %in% colnames(df)) {stop("There must be a variable with 'age'")}
+  check_column_in_df(df, core)
+  check_column_in_df(df, mind)
+  check_column_in_df(df, maxd)
+  check_column_in_df(df, dbd)
+  check_column_in_df(df, oc)
+  check_column_in_df(df, age)
 
   # class of the columns
-  if (!is.numeric(df[[dmin]])) {stop("Minimum depth data is not class numeric, please check")}
-  if (!is.numeric(df[[dmax]])) {stop("Maximum depth data is not class numeric, please check")}
+  if (!is.numeric(df[[mind]])) {stop("Minimum depth data is not class numeric, please check")}
+  if (!is.numeric(df[[maxd]])) {stop("Maximum depth data is not class numeric, please check")}
   if (!is.numeric(df[[dbd]])) {stop("Dry Bulk Density data is not class numeric, please check")}
   if (!is.numeric(df[[oc]])) {stop("Organic carbon data is not class numeric, please check")}
   if (!is.numeric(df[[age]])) {stop("Age data is not class numeric, please check")}
@@ -51,75 +51,78 @@ estimate_flux<- function(df=NULL,
   # create variables with working names with the data in the columns specified by the user
   df_r <- df
   df_r$core_r <- df_r[[core]]
-  df_r$dmin_r <- df_r[[dmin]]
-  df_r$dmax_r <- df_r[[dmax]]
+  df_r$mind_r <- df_r[[mind]]
+  df_r$maxd_r <- df_r[[maxd]]
   df_r$dbd_r <- df_r[[dbd]]
   df_r$oc_r <- df_r[[oc]]
   df_r$age_r <- df_r[[age]]
 
 
   #select those cores with chronological models
-  df_r<-df_r[!is.na(df_r$Age_r),]
+  df_r<-df_r[!is.na(df_r$age_r),]
   df_r<-df_r[!is.na(df_r$oc_r),]
 
   df_h<-estimate_h (df_r)
 
-  x<-split(df_h, df_h$core_r)
 
-  #BCF <- data.frame(CoreID=character(),
-   #                 F.WC=numeric(),
-    #                A.Max=numeric(),
-     #               Flux=numeric())
+  # estimate fluxes
+  x <- split(df_h, df_h$core_r)
+  BCF_l <- lapply(X = x,  estimate_core_f, timeframe = timeframe) # return a list
+  BCF <- as.data.frame(do.call(rbind, BCF_l)) # from list to dataframe
 
+  rownames(BCF) <- NULL
 
-  for(i in 1:length(X)) {
-    BCF[i,1]<-names(X[i])
-    Data<-as.data.frame(X[i])
-    colnames(Data)<-colnames(df)
-
-    estimate_core_f<- function (df, timeframe) {
-
-      CoreID <- as.character(unique(df[1,"core_r"]))
-      df<-as.data.frame(df)
-      colnames(df)<-colnames(df_h)
-
-
-
-
-
-
-
-      }
-
-
-
-
-
-    if(nrow(Data)<3) next
-
-    else{
-
-      Data <-Data |> dplyr::mutate (OCgcm2 = DBD*(fOC/100)*h)
-
-      #estimation of the average carbon flux for the whole core (OC stock/Max Age)
-      BCF[i,2]<-(sum(Data[,which(colnames(Data)=="OCgcm2")]))/max(Data$Age)
-      BCF[i,3]<-max(Data$Age)
-
-      #estimation of the average carbon flux for the selected TimeFrame (OC stock last 100 yrs/TimeFrame)
-
-      if (max(Data$Age)==TimeFrame) {
-
-        BCF[i,4]<-((sum(Data[c(1:(nrow(Data))),which(colnames(Data)=="OCgcm2")]))/TimeFrame)
-
-      } else {
-        Data<-Data[c(1:(length(which(Data$Age <=TimeFrame))+1)),]
-
-        BCF[i,4]<-(
-          (sum(Data[c(1:(nrow(Data)-1)),which(colnames(Data)=="OCgcm2")])+
-                       (Data[nrow(Data),which(colnames(Data)=="OCgcm2")]/((max(Data$Age)-Data[(nrow(Data)-1),which(colnames(Data)=="Age")])))
-                        *(TimeFrame-Data[(nrow(Data)-1),which(colnames(Data)=="Age")]))/TimeFrame)
-      }
-
-    }}
   return(BCF)
+
 }
+
+
+
+
+estimate_core_f<- function (df, timeframe) {
+
+  core <- as.character(unique(df[1,"core_r"]))
+
+  #colnames(df)<-colnames(df_h)
+
+  if (is.unsorted(df$mind_r)) {stop("Samples must be ordered from shallow to deep")}
+
+  #estimation of carbon g cm2 per sample, OCgcm2= carbon density (g cm3) by thickness (h)
+  df$ocgcm2 <- df$dbd_r * (df$oc_r / 100) * df$h
+
+  #estimation of the OC stock in the whole core
+  fluxwc <- sum(df$ocgcm2)/max(df$age_r)
+  maxage <- max(df$age_r)
+
+  #estimation of the average carbon flux for the selected TimeFrame (OC stock / TimeFrame)
+
+  if (max(df$age_r)==timeframe) {
+
+    flux<-sum(df$ocgcm2)/max(df$age_r)
+
+  } else {
+    df<-df[c(1:(length(which(df$age_r <=timeframe))+1)),]
+
+    flux<-(
+      (sum(df[c(1:(nrow(df) - 1)), "ocgcm2"])+
+         ((df[nrow(df),"ocgcm2"]/((max(df$age_r)-df[(nrow(df)-1),"age_r"])))
+          *(timeframe-df[(nrow(df)-1),"age_r"])))/timeframe)
+  }
+
+  BCF <- data.frame(core = core, fluxwc = fluxwc, maxage = maxage, flux = flux)
+
+  return(BCF)
+
+}
+
+
+estimate_flux(df_f,
+                         timeframe=100,
+                         core = "core",
+                         mind = "mind",
+                         maxd = "maxd",
+                         dbd = "dbd",
+                         oc = "eoc",
+                         age = "age")
+
+
