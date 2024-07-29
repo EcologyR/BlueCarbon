03f6121dcccbd59936f1445ca30c75a2373df920
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-29 11:53:11 +0200

    don't check df in check_column

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,6 @@
 #' @noRd
 check_column_in_df <- function(df = NULL, colname = NULL) {
 
-  if (!inherits(df, "data.frame")) {
-    stop("df is not a tibble or data.frame.")
-  }
-
   if (!is.character(colname)) {
     stop(colname, " is not a character. Please provide the column name as character.")
   }
