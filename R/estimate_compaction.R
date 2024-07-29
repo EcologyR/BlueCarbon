a1703c01b9d53e5fed2bb58ea6ea3fbd9773f786
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-29 11:53:28 +0200

    revised estimate_compaction

--- R/estimate_compaction.R
+++ R/estimate_compaction.R
@@ -18,10 +18,15 @@
 #' the estimated percentage of core compaction.
 #'
 #' @examples
+#' df <- estimate_compaction(core_comp,
+#' core_id = "core",
+#' sampler_length = "sampler_length",
+#' internal_distance = "internal_distance",
+#' external_distance = "external_distance")
 #'
 #' @export
 #'
-estimate_compaction <- function(df,
+estimate_compaction <- function(df = NULL,
                                 core_id = "core",
                                 sampler_length = "sampler_length",
                                 internal_distance = "internal_distance",
@@ -49,10 +54,19 @@ estimate_compaction <- function(df,
     stop("Column 'external_distance' must be numeric.")
   }
 
+
+  ## Omit missing data
+  complete <- complete.cases(df)
+  if (!all(complete)) {
+    warning("Removing cores with missing data: ", df$core[!(complete)])
+    df <- df[complete.cases(df), ]
+  }
+
   # Check that internal distance is greater than or equal to external distance
   if (any(df[[internal_distance]] < df[[external_distance]])) {
     stop("Internal distance cannot be smaller than external distance")
   }
+
 
   # Create working copies of columns
   df$core_r <- df[[core_id]]
@@ -67,6 +81,13 @@ estimate_compaction <- function(df,
 
   # Calculate compaction rate as percentage
   df$compression <- (1 - compaction_correction_factor) * 100
+
+  if (any(df$compression < 0 | df$compression > 100)) {
+    stop("Estimated compression below 0 or above 100: please check cores ",
+         df$core[df$compression < 0 | df$compression > 100])
+  }
+
+  df <- subset(df, select = -c(core_r, sampler_length_r, internal_distance_r, external_distance_r))
 
   return(df)
 }
--- tests/testthat/test-estimate_compaction.R
+++ tests/testthat/test-estimate_compaction.R
@@ -11,9 +11,6 @@ test_that("estimate_compaction function works as expected", {
   # Test if the function runs without error
   expect_no_error(estimate_compaction(df))
 
-  # Test if the output data frame has the correct number of rows
-  expect_equal(nrow(estimate_compaction(df)), nrow(df))
-
   # Test if the output data frame contains the compression column
   expect_true("compression" %in% names(estimate_compaction(df)))
 
@@ -24,7 +21,7 @@ test_that("estimate_compaction function works as expected", {
 
   # Test if the function throws an error when non-numeric values are provided
   df$sampler_length <- as.character(df$sampler_length)
-  expect_error(estimate_compaction(df), "'sampler_length' data must be class numeric")
+  expect_error(estimate_compaction(df))
 
   # Test if the function throws an error when internal_distance is smaller than external_distance
   df <- data.frame(
@@ -33,5 +30,6 @@ test_that("estimate_compaction function works as expected", {
     internal_distance = c(5, 10, 5),  # internal_distance < external_distance for the third row
     external_distance = c(2, 5, 10)
   )
-  expect_error(estimate_compaction(df), "internal_distance is smaller than the external_distance")
+  expect_error(estimate_compaction(df))
+
 })
